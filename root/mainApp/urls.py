6c58b120a41c0f330be261301c7407875b94ac74
Shubh Sehgal <shubh.sehgal2506@gmail.com>
2022-07-05 01:32:49 +0530

    understanding the basics

--- root/mainApp/urls.py
+++ root/mainApp/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects/',views.projects,name="projects")
+    path('projects/',views.projects,name="projects"),
+    path('',views.mainpage,name="mainpage"),
+    path('project/<int:pk>/',views.project,name="singleproject"),
+
 ]
--- root/mainApp/views.py
+++ root/mainApp/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 def projects(request):
     return render(request,'projects.html')
+
+def mainpage(request):
+    return render(request,'main.html')
+
+
+def project(request,pk):
+    return render(request,'single-project.html')
