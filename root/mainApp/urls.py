2cf10fafcee2a4184125a6870bc35ae707679508
Shubh Sehgal <shubh.sehgal2506@gmail.com>
2022-07-08 22:08:43 +0530

    fixing bugs

--- root/mainApp/urls.py
+++ root/mainApp/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects/',views.projects,name="projects"),
-    path('',views.mainpage,name="mainpage"),
+    path('',views.projects,name="projects"),
+    #path('',views.mainpage,name="mainpage"),
     path('project/<int:pk>/',views.project,name="singleproject"),
 
 ]
--- root/mainApp/views.py
+++ root/mainApp/views.py
@@ -1,12 +1,34 @@
+from re import L
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from numpy import number
+project_list = [
+    {'id':1,
+    'title':"Ecommerce Website",
+    'description':'Fully functional ecommerce website'},
+    { 'id':2,
+    'title':"Portfolio Website",
+    'description':'This wasaproject whereIbuilt out my portfolio'},
+    {'id':3,
+    'title':"Social Network",
+    'description':'Awesome open source projectIam still working on'}
+]
+    
+   
+    
 # Create your views here.
 def projects(request):
-    return render(request,'mainApp/projects.html')
+    msg = 'hello, you are on projects page'
+    number = 11
+    context = {'msg':msg,'number':number,'projectlist':project_list}
+    return render(request,'mainApp/projects.html',context)
 
 def mainpage(request):
     return render(request,'main.html')
 
 def project(request,pk):
-    return render(request,'mainApp/single-project.html')
+    proj = None
+    for i in project_list:
+        if i['id'] == pk:
+            proj = i
+    return render(request,'mainApp/single-project.html',{'project':proj})
