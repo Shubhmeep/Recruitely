4c23469d603cb59af24cac62b702971ea480342d
Shubh Sehgal <shubh.sehgal2506@gmail.com>
2022-07-09 15:36:55 +0530

    3 models added

--- root/mainApp/admin.py
+++ root/mainApp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import project
+from .models import project,review,tag
 # Register your models here.
 admin.site.register(project)
+admin.site.register(review)
+admin.site.register(tag)
--- root/mainApp/models.py
+++ root/mainApp/models.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 import uuid
 from django.db import models
 import uuid
@@ -8,9 +9,32 @@ class project(models.Model):
     description = models.TextField(null=True,blank=True)
     demo_link = models.CharField(max_length=200)
     source_link = models.CharField(max_length=200)
+    tags = models.ManyToManyField('tag',blank=True)
     vote_ratio = models.IntegerField(default=0, null = True, blank=True)
     vote_total = models.IntegerField(default=0, null = True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return str(self.title)
+
+
+class review(models.Model):
+    id = models.UUIDField(default=uuid.uuid4,primary_key=True,unique=True,editable=False)
+    project = models.ForeignKey(project,on_delete=models.CASCADE)
+    VOTE_TYPE = (
+        ('up','Up Vote'),
+        ('down','Down Vote')
+    )
+    body = models.TextField(null=True,blank=True)
+    value = models.CharField(max_length=200,choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    def __str__(self) -> str:
+        return str(self.value)
+
+class tag(models.Model):
+    id = models.UUIDField(default=uuid.uuid4,primary_key=True,unique=True,editable=False)
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return str(self.name)
